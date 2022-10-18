a6b06823d08711876b07123067e1b84128088354
AndreaDeCeglia <deceglia89@gmail.com>
2022-10-18 15:34:13 +0200

    second variable set

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,13 @@ let name = prompt('write your name');
 //just check it
 
 console.log(name);
+
+//setting of second variable
+//about LAST-NAME
+
+let lastname = prompt('write your lastname');
+
+//check
+
+console.log(lastname);
+
